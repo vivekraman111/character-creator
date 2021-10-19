1fc58350dd3b67443027b18d25f4b30d7cb64227
vivekraman <vivekraman111@gmail.com>
2021-10-19 13:40:29 +0530

    initial changes

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -31,6 +31,7 @@ function App() {
   return (
     <main className={styles.characterEditor}>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        <div className={styles.grayBar} />
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
